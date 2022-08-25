9ef432f67b59747ff78142e66fc4b378bae51d72
klarutkow <klaudiarutkowska@poczta.fm>
2022-08-25 11:49:37 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'db3140e2-2ac7-42a7-ac50-954dec416a49';
+export const MODAL_CLOSE_SEC = 2;
+
+//API doc:
+//https://forkify-api.herokuapp.com/v2
